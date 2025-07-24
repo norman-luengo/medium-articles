e32c0d63de685e8df43709e41e7150ccca5b4d6e
norman-luengo <luengo.norman@gmail.com>
2025-07-24 23:32:56 +0200

    refactor: remove agent file to simplify logic; now Agent is called directly without wrapper classes

--- agentic_mlflow_article/app/main.py
+++ agentic_mlflow_article/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from schemas import QuestionRequest, AnswerResponse
 from agent import LocalModelAgent
+from pydantic_ai import Agent
 
 app = FastAPI()
 
@@ -12,6 +13,9 @@ async def chat_agent(payload: QuestionRequest):
     if not question:
         raise HTTPException(status_code=400, detail="Question cannot be empty.")
 
-    agent = LocalModelAgent()
-    answer = agent.get_response(question)
-    return AnswerResponse(answer=answer)
+    agent = Agent(
+            'google-gemini:gemini-1.5-flash',
+            system_prompt='You are a helpful assistant. Answer concisely.'
+        )
+    result = await agent.run(question)
+    return AnswerResponse(answer=result.output)
