db191c69b282fd8b714fce57fe131f4675ddadb0
norman-luengo <luengo.norman@gmail.com>
2025-07-25 22:35:40 +0200

    fix: add git and bin to final image of the dockerfile. feat: add ollama model provider and mlflow monitoring

--- agentic_mlflow_article/app/main.py
+++ agentic_mlflow_article/app/main.py
@@ -1,7 +1,10 @@
+
 from fastapi import FastAPI, HTTPException
-from schemas import QuestionRequest, AnswerResponse
-from agent import LocalModelAgent
+from app.schemas import QuestionRequest, AnswerResponse
 from pydantic_ai import Agent
+import mlflow
+from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.openai import OpenAIProvider
 
 app = FastAPI()
 
@@ -12,10 +15,20 @@ async def chat_agent(payload: QuestionRequest):
 
     if not question:
         raise HTTPException(status_code=400, detail="Question cannot be empty.")
-
+    
+    ollama_model = OpenAIModel(
+    model_name='deepseek-r1:32b', provider=OpenAIProvider(base_url='http://localhost:11434/v1')
+)
+    system_prompt = 'You are a helpful assistant. Answer concisely.'
     agent = Agent(
-            'google-gemini:gemini-1.5-flash',
-            system_prompt='You are a helpful assistant. Answer concisely.'
-        )
-    result = await agent.run(question)
-    return AnswerResponse(answer=result.output)
+        ollama_model,
+        system_prompt=system_prompt
+    )
+
+    with mlflow.start_run(run_name="chat_agent_interaction"):
+        mlflow.log_param("agent_model", ollama_model)
+        mlflow.log_param("system_prompt", system_prompt)
+        mlflow.log_param("question", question)
+        result = await agent.run(question)
+        mlflow.log_param("answer", result.output)
+        return AnswerResponse(answer=result.output)
